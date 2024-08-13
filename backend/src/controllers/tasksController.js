6f98a918d616f9a4848701794e5a026172f66281
Helber Dias <98468468+HelberDiasDEV@users.noreply.github.com>
2024-08-13 17:55:27 -0300

    [ADD] metodo post

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,10 +1,17 @@
 const tasksModel = require('../models/tasksModel');
 
-const getAll = async (request, response) => {
+const getAll = async (_req, res) => {
     const tasks = await tasksModel.getAll();
-    return response.status(200).json(tasks);
+    return res.status(200).json(tasks);
+};
+
+const createTasks = async (req, res) => {
+    const createdTasks = await tasksModel.createTasks(req.body);
+   
+    return res.status(201).json(createdTasks);
 };
 
 module.exports = {
-    getAll
+    getAll,
+    createTasks
 };
